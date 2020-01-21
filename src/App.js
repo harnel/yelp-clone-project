41a75275c280b0a8c8b9f537bf8198db495ffa00
Yungbae Park <ybpark54@gmail.com>
2020-01-21 21:31:01 +0900

    'lll'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-
+import LandingPage from './LandingPage/LandingPage';
 
 function App() {
   return (
-    <div>Aappppppppp</div>
+    <LandingPage />
   )
 }
 
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TopNav from '../TopNav/TopNav';
+
+import './LandingPage.module.css';
+
+const LandingPage = props =>{
+    return (
+        <TopNav />
+    )
+}
+
+export default LandingPage;
