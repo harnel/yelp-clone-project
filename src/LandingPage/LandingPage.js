03260732969d30869fca7264f27a50003d461c9b
Yungbae Park <ybpark54@gmail.com>
2020-01-23 23:00:19 +0900

    adapt layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -8,12 +8,15 @@ import styles from './LandingPage.module.css';
 
 const LandingPage = props =>{
     return (
-        <div>
-            <TopNav />
-            <img src={logo} className={styles['logo']} alt='logo'/>
-            <SearchBar />
-            <SearchSuggestions />
+        <div className={styles['landing']}>
+            <div className={styles['search-area']}>
+                <TopNav />
+                <img src={logo} className={styles['logo']} alt='logo'/>
+                <SearchBar />
+                <SearchSuggestions />
+            </div>
         </div>
+        
         
     )
 }
